8807ee0a8581f55f8ec2de4b95d9c7be29e20c1f
eli <elicuro96@gmail.com>
2022-12-13 16:35:46 -0500

    dia 13/12/

--- user/views.py
+++ user/views.py
@@ -4,5 +4,5 @@ from .models import Post
 # Create your views here.
 
 def render_post(request):
-    post = Post.objects.all()
-    return render(request, 'post.html')
+    posts = Post.objects.all()
+    return render(request,'post.html', {'posts':posts})
