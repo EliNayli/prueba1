cfc597a00e2316899f16cf1acd8e6f45afc1d07d
eli <elicuro96@gmail.com>
2022-12-12 19:08:04 -0500

    second

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,13 +3,9 @@ from django.db.models.fields import CharField, URLField
 from django.db.models.fields.files import ImageField
 
 class Project(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.CharField(max_length=700)
-    image = models.ImageField(upload_to='portfolio/images')
-    url = models.URLField(blank=True)
-
-    class Meta:
-        verbose_name = 'Project'
-        verbose_name_plural = 'Projects'
+    title = CharField(max_length=100)
+    description = CharField(max_length=700)
+    image = ImageField(upload_to='portfolio/images/')
+    url = URLField(blank=True)
 
   
--- user/views.py
+++ user/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def render_post(request):
+    post = Post.objects.all()
+    return render(request, 'post.html')
